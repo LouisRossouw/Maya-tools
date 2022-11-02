bb27dca628c59902617cf0d9ab4a1ec777ff6637
LouisRossouw <80905013+LouisRossouw@users.noreply.github.com>
2022-11-02 15:27:06 +0200

    Update RandomSelect.py

--- scripts/RandomSelect.py
+++ scripts/RandomSelect.py
@@ -1,14 +1,11 @@
 """
-Randomly animates objects dissearing in a randomised way, good for pixle art fade out
+Randomly select objects.
 """
 
 import random
 import maya.cmds as cmds
 
-
 amount_to_divide = 2
-
-
 
 sel = cmds.ls(selection=True)
 selection_children = cmds.listRelatives(sel, ad=True)
@@ -17,7 +14,6 @@ obj_list = []
 for obj in selection_children:
 	if "Shape" not in obj:
 		obj_list.append(obj)
-
 
 count_objects = len(obj_list)
 amount_to_select = count_objects / amount_to_divide
